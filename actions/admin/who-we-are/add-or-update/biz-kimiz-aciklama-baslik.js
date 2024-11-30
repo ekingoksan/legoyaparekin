5e297ccebb83444aa3b9fbd40bfea66f79e8c32c
Sezer <sezer.boluk@niltekyazilim.com.tr>
2024-11-30 14:46:11 +0300

    latests

--- actions/admin/who-we-are/add-or-update/biz-kimiz-aciklama-baslik.js
+++ actions/admin/who-we-are/add-or-update/biz-kimiz-aciklama-baslik.js
@@ -39,3 +39,64 @@ export const addOrUpdateeBizKimizAciklamaBaslik = async ({
     };
   }
 };
+
+
+export const addServices = async (services) => {
+
+  if(!services || services.length === 0) {
+    return {
+      status: 400,
+      message: "Hizmetler boş olamaz"
+    }
+  }
+
+
+  // remove all services
+  await db.ourSkills.deleteMany({});
+
+
+  services.map(async (service) => {
+    const result = await db.ourSkills.create({
+      data: {
+        title: service.name,
+      }
+    })
+  })
+
+  return {
+    status: 200,
+    message : "Hizmetler başarıyla eklendi"
+  }
+
+}
+
+export const getServices = async () => {
+  const services = await db.ourSkills.findMany({});
+
+  return services;
+}
+
+export const deleteService = async (id) => {
+
+
+  const exists = await db.ourSkills.findFirst({
+    where: {
+      id
+    }
+  });
+
+  if(!exists) {
+   return {}
+  }
+  
+  const result = await db.ourSkills.delete({
+    where: {
+      id
+    }
+  });
+
+  return {
+    status: 200,
+    message: "Hizmet başarıyla silindi"
+  }
+}
--- components/admin/biz-kimiz/yeteneklerimiz.jsx
+++ components/admin/biz-kimiz/yeteneklerimiz.jsx
@@ -1,79 +1,147 @@
 "use client";
 
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { useForm } from "react-hook-form";
-import { SunEditorComponent } from "@/components/ui/suneditor";
+import { useForm, useFieldArray } from "react-hook-form";
 import { useToast } from "@/hooks/use-toast";
-import {addOrUpdateeBizKimizAciklamaBaslik} from '@/actions/admin/who-we-are/add-or-update/biz-kimiz-aciklama-baslik'
-import { getWhoWeAre } from "@/actions/admin/who-we-are/get/get-who-we-are";
-import { PlusIcon } from "lucide-react";
+import { PlusIcon, TrashIcon } from "lucide-react";
+import {
+  addServices,
+  deleteService,
+  getServices,
+} from "@/actions/admin/who-we-are/add-or-update/biz-kimiz-aciklama-baslik";
+
 function Yeteneklerimiz() {
   const { toast } = useToast();
+  const [services, setServices] = React.useState([]);
+
   const form = useForm({
     defaultValues: {
-      title: "",
-      description: "",
+      services: [{ name: "", id: "" }],
     },
   });
 
+  useEffect(() => {
+    form.setValue(
+      "services",
+      services?.map((service) => ({ name: service.title, id: service.id }))
+    );
+  }, [services]);
+
+  const { fields, append, remove } = useFieldArray({
+    control: form.control,
+    name: "services",
+  });
+
+  const fetchServices = async () => {
+    const response = await getServices();
+    setServices(response);
+  };
+
+  useEffect(() => {
+    fetchServices();
+  }, []);
+
   const onSubmit = async (data) => {
-    const result = await addOrUpdateeBizKimizAciklamaBaslik(data);
+    const result = await addServices(data.services);
+
     if (result.status === 200) {
       toast({
-        title: 'Başarılı',
+        title: "Başarılı",
         description: result.message,
-        variant: 'success',
+        variant: "success",
         duration: 2000,
       });
+
+      fetchServices();
+
     } else {
       toast({
-        title: 'Hata',
+        title: "Hata",
         description: result.message,
-        variant: 'destrutive',
+        variant: "destrutive",
+        duration: 2000,
+      });
+      
+    }
+  };
+
+
+  const _deleteService = async (id) => {
+    // deleteService
+    const result = await deleteService(id);
+
+    if (result.status === 200) {
+      toast({
+        title: "Başarılı",
+        description: result.message,
+        variant: "success",
+        duration: 2000,
+      });
+
+      fetchServices();
+    }
+
+    if (result?.status === 400) {
+      toast({
+        title: "Hata",
+        description: result.message,
+        variant: "destrutive",
         duration: 2000,
       });
     }
   };
 
-  const getData = async () => {
-   const result =  await getWhoWeAre()
-
-   if(result) {
-        form.setValue('title', result.title)
-        form.setValue('description', result.description)
-   }
-  }
-
-  useEffect(() => {
-    getData()
-  }, [])
+  console.log(services, "services");
 
   return (
     <form onSubmit={form.handleSubmit(onSubmit)}>
       <div className="flex flex-col gap-1 mb-5">
-        <Label>Hizmet Ekle</Label>
-       <div className="relative">
-       <Input
-          type="text"
-          name="title"
-          {...form.register("title")}
-          placeholder=""
-          className="w-full"
-        />
-        <div className=" absolute right-2 top-1/2 transform -translate-y-1/2  border p-1 rounded 
-        hover:bg-primary-500  cursor-pointer
-        ">
-       <PlusIcon className="w-6 h-6 text-primary-500 shrink-0" />
-
+        <div className="flex justify-between">
+          <Label>Hizmet Ekle</Label>
+          <Button size="sm" type="button" onClick={() => append({ name: "" })}>
+            <PlusIcon className="w-6 h-6 text-primary-500 shrink-0" />
+          </Button>
         </div>
+        {fields.map((field, index) => (
+          <div key={field.id}>
+            <div className="relative mb-2">
+              <Input
+                type="text"
+                name={`services[${index}].name`}
+                {...form.register(`services.${index}.name`, {
+                  required: "Bu alan zorunludur",
+                })}
+                placeholder={`Hizmet ${index + 1}`}
+                className="w-full"
+              />
 
-       </div>
+              <Button
+                variant="outline"
+                type="button"
+                className="absolute right-0 top-1/2 transform -translate-y-1/2"
+                onClick={() => {
+                  _deleteService(services[index].id);
+                  remove(index);
+                }}
+              >
+                <TrashIcon className="w-4 h-4 text-red-500" />
+              </Button>
+            </div>
 
+            <div>
+              {form.formState.errors.services?.[index]?.name && (
+                <p className="text-red-500 text-sm">
+                  {form.formState.errors.services[index].name.message}
+                </p>
+              )}
+            </div>
+          </div>
+        ))}
       </div>
-   
+
       <div className="flex justify-end mt-5">
         <Button variant="primary" size="lg">
           Kaydet
